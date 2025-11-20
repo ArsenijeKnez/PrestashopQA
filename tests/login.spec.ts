05f313ffbdcc74d524c201062ab01abe2e8bfa01
ArsenijeKnez <arsenije.knezevic6@gmail.com>
2025-11-20 12:38:20 +0100

    Navigation tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,10 +3,25 @@ import { test, expect } from "@playwright/test";
 //PrestaShop demo URL with english language
 const DEMO_URL = "https://demo.prestashop.com/#/en/front";
 
+//Testing navigation to the login form
+test("Navigation to login", async ({ page }) => {
+  await page.goto(DEMO_URL);
+
+  const frame = page.frameLocator('//*[@id="framelive"]');
+  await expect(frame.locator('span:has-text("Sign in")')).toBeVisible({
+    timeout: 30000,
+  });
+
+  await frame.locator('span:has-text("Sign in")').click();
+
+  //Verification  of login form visibility
+  await expect(frame.locator('//*[@id="login-form"]')).toBeVisible();
+});
+
 //Test login functionality
 test("User can login successfully", async ({ page }) => {
+  //Navigate to demo site and login page
   await page.goto(DEMO_URL);
-  //prestashop uses iframe
   const frame = page.frameLocator('//*[@id="framelive"]');
 
   await frame.locator('span:has-text("Sign in")').click();
@@ -18,15 +33,14 @@ test("User can login successfully", async ({ page }) => {
 
   //Filling in data
   await frame.locator("#field-id_gender-1").check();
-
   const email = `autotest_${Date.now()}@mail.com`;
-
   await frame.locator("#field-firstname").fill("AG");
   await frame.locator("#field-lastname").fill("Tester");
   await frame.locator("#field-email").fill(email);
   await frame.locator("#field-password").fill("QATest555!");
   await frame.locator("#field-birthday").fill("01/01/1990");
 
+  //Requirements
   await frame.locator('input[name="psgdpr"]').check();
   await frame.locator('input[name="customer_privacy"]').check();
 
@@ -38,27 +52,29 @@ test("User can login successfully", async ({ page }) => {
     timeout: 30000,
   });
 
+  //Sign out
   await frame.locator('a:has-text(" Sign out")').click();
   //await frame.locator('//*[@id="_desktop_user_info"]/div/a[1]').click();
 
+  //Now we can test login with the registered user
   await frame.locator('span:has-text("Sign in")').click();
-
+  //Filling in login data
   await frame.locator("#field-email").fill(email);
   await frame.locator("#field-password").fill("QATest555!");
   await frame.locator("#submit-login").click();
-
+  //Verification of successful login
   const logoutLocator = frame.locator(".user-info .logout");
   const accountLocator = frame.locator(".user-info a.account");
 
   await expect(logoutLocator).toBeVisible({ timeout: 30000 });
   await expect(accountLocator).toBeVisible({ timeout: 30000 });
 
+  //Additional verification of user name
   const userInfo = frame.locator("#_desktop_user_info");
   await expect(userInfo).toBeVisible();
   await expect(userInfo.locator(".user-info")).toContainText(" Sign out ");
   await expect(userInfo.locator(".user-info")).toContainText("AG Tester");
 });
-
 
 //Testing invalid login
 test("User cannot login with invalid credentials", async ({ page }) => {
@@ -68,14 +84,15 @@ test("User cannot login with invalid credentials", async ({ page }) => {
 
   await frame.locator('span:has-text("Sign in")').click();
 
+  //Filling in non-existing user data
   await frame.locator("#field-email").fill("invalid@gmail.com");
   await frame.locator("#field-password").fill("NoPass111!");
   await frame.locator("#submit-login").click();
 
+  //Verification of error message
   const errorMessage = frame.locator("div.help-block li.alert.alert-danger");
   await expect(errorMessage).toHaveText("Authentication failed.");
 });
-
 
 //Testing forgot password functionality(accepting any valid email)
 test("Forgot password recovery success", async ({ page }) => {
@@ -84,6 +101,8 @@ test("Forgot password recovery success", async ({ page }) => {
   const frame = page.frameLocator('//*[@id="framelive"]');
 
   await frame.locator('span:has-text("Sign in")').click();
+
+  //Navigate to forgot password
   const forgotLink = frame.getByRole("link", { name: "Forgot your password?" });
   await expect(forgotLink).toBeVisible({
     timeout: 30000,
@@ -93,13 +112,16 @@ test("Forgot password recovery success", async ({ page }) => {
   await expect(resetButton).toBeVisible({
     timeout: 30000,
   });
+
+  //Filling in email and submitting
   frame.locator("#email").fill("rand@mail.com");
   await resetButton.click();
+
+  //Verification of success message
   await expect(frame.locator("#content")).toHaveText(
     "If this email address has been registered in our store, you will receive a link to reset your password at rand@mail.com."
   );
 });
-
 
 //Testing forgot password with invalid email
 test("Forgot password ivalid email", async ({ page }) => {
@@ -117,31 +139,14 @@ test("Forgot password ivalid email", async ({ page }) => {
   await expect(resetButton).toBeVisible({
     timeout: 30000,
   });
+
+  //Filling in invalid email and submitting
   frame.locator("#email").fill("rand@a");
   await resetButton.click();
+
+  //Verification of error message
   const errorLocator = frame.getByText("Invalid email address.", {
     exact: true,
   });
-
   await expect(errorLocator).toBeVisible();
-});
-
-
-//Testing redirect to registration page from login
-test("Redirect to registration page works", async ({ page }) => {
-  await page.goto(DEMO_URL);
-
-  const frame = page.frameLocator('//*[@id="framelive"]');
-  await expect(frame.locator('span:has-text("Sign in")')).toBeVisible({
-    timeout: 30000,
-  });
-
-  await frame.locator('span:has-text("Sign in")').click();
-  //await frame.locator('//*[@id="_desktop_user_info"]/div/a').click();
-
-  frame.getByRole("link", { name: "No account? Create one here" }).click();
-
-  await expect(frame.locator('//*[@id="main"]/header/h1')).toHaveText(
-    "Create an account"
-  );
 });
--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -10,24 +10,58 @@ function getFieldErrorLocator(
 ): Locator {
   const inputElement = iframeLocator.locator(fieldId);
 
+  //Finding the nearest common ancestor
   const fieldContainer = inputElement.locator(
     'xpath=ancestor::div[contains(@class, "form-group row")]'
   );
 
+  //Finding the error message within that ancestor
   return fieldContainer.locator(".help-block li.alert-danger");
 }
+
+//Testing redirect to registration page from login
+test("Redirect to registration page and back to login works", async ({
+  page,
+}) => {
+  await page.goto(DEMO_URL);
+
+  const frame = page.frameLocator('//*[@id="framelive"]');
+  await expect(frame.locator('span:has-text("Sign in")')).toBeVisible({
+    timeout: 30000,
+  });
+
+  await frame.locator('span:has-text("Sign in")').click();
+  //await frame.locator('//*[@id="_desktop_user_info"]/div/a').click();
+
+  //Click on create account link
+  frame.getByRole("link", { name: "No account? Create one here" }).click();
+
+  //Verification of redirection to registration page
+  /*   await expect(frame.locator('//*[@id="main"]/header/h1')).toHaveText(
+    "Create an account"
+  ); */
+  await expect(frame.locator('//*[@id="customer-form"]')).toBeVisible();
+
+  //Now test redirection back to login page
+  frame.getByRole("link", { name: "Log in instead!" }).click();
+
+  //Verification of redirection to login page
+  await expect(frame.locator('//*[@id="login-form"]')).toBeVisible();
+});
 
 //Test successful registration
 test("User can register successfully", async ({ page }) => {
   await page.goto(DEMO_URL);
   const frame = page.frameLocator('//*[@id="framelive"]');
 
+  //Navigate to registration page
   await frame.locator('span:has-text("Sign in")').click();
 
   await frame
     .getByRole("link", { name: "No account? Create one here" })
     .click();
 
+  //Creating a new user
   await frame.locator("#field-id_gender-1").check();
 
   const email = `autotest_${Date.now()}@mail.com`;
@@ -47,6 +81,7 @@ test("User can register successfully", async ({ page }) => {
     timeout: 30000,
   });
 
+  //Verification of successful registration
   const logoutLocator = frame.locator(".user-info .logout");
   const accountLocator = frame.locator(".user-info a.account");
 
@@ -72,6 +107,7 @@ test("User cannot register with the same email", async ({ page }) => {
     .getByRole("link", { name: "No account? Create one here" })
     .click();
 
+  //Creating a new user
   await frame.locator("#field-id_gender-1").check();
 
   const email = `autotest_${Date.now()}@mail.com`;
@@ -100,6 +136,7 @@ test("User cannot register with the same email", async ({ page }) => {
     .getByRole("link", { name: "No account? Create one here" })
     .click();
 
+  //Trying to register with the same email
   await frame.locator("#field-id_gender-1").check();
 
   await frame.locator("#field-firstname").fill("Novi");
@@ -113,6 +150,7 @@ test("User cannot register with the same email", async ({ page }) => {
 
   frame.locator('button[data-link-action="save-customer"]').click();
 
+  //Verification of error message
   const emailError = getFieldErrorLocator(frame, "#field-email");
 
   await expect(emailError).toBeVisible({ timeout: 40000 });
@@ -120,6 +158,7 @@ test("User cannot register with the same email", async ({ page }) => {
     "The email is already used, please choose another one or sign in"
   );
 
+  //User is not signed in
   const userInfo = frame.locator("#_desktop_user_info");
   await expect(userInfo.locator(".user-info")).toContainText(" Sign in ");
 });
@@ -135,10 +174,8 @@ test("User cannot register with invalid data", async ({ page }) => {
     .getByRole("link", { name: "No account? Create one here" })
     .click();
 
+  //Filling in invalid data
   await frame.locator("#field-id_gender-1").check();
-
-  //const email = `autotest_${Date.now()}@mail.com`;
-
   await frame.locator("#field-firstname").fill("1");
   await frame.locator("#field-lastname").fill("2");
   await frame.locator("#field-email").fill("random@mail.rs");
@@ -150,6 +187,7 @@ test("User cannot register with invalid data", async ({ page }) => {
 
   frame.locator('button[data-link-action="save-customer"]').click();
 
+  //Verification of error messages
   const firstNameError = getFieldErrorLocator(frame, "#field-firstname");
   const lastNameError = getFieldErrorLocator(frame, "#field-lastname");
   const birthdayError = getFieldErrorLocator(frame, "#field-birthday");
@@ -159,6 +197,7 @@ test("User cannot register with invalid data", async ({ page }) => {
   await expect(lastNameError).toHaveText("Invalid format.");
   await expect(birthdayError).toHaveText("Format should be 05/31/1970.");
 
+  //User is not signed in
   const userInfo = frame.locator("#_desktop_user_info");
   await expect(userInfo.locator(".user-info")).toContainText(" Sign in ");
 });
